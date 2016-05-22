cf73aa6a35bdac0f45f369631d5f04b1e246c3ac
Mark Puchala II <markpuchalaii@gmail.com>
2016-05-22 15:38:32 -0500

    Add Binary Calculator (Proof of Concept)
    Added the proof of Concept files for my Binary Calculator, as well as the original help files I made for it before I knew what a proof of Concept was.
    
    Next project will be separating these two.

---
+++ Binary Calculator/functions.js
@@ -0,0 +1,82 @@
+//This function computes all textboxes based upon which one you pressed Enter on.
+// The function works as follows: compute(input box's ID, Input type's base number)
+
+//Declare a variable to track which input was most recently used
+var input = ['binary', 'octal', 'decimal', 'hexadecimal'];
+var label;
+
+/******************************************************
+ *******  compute() updates all the fields      *******
+ *******  based on which is being used          *******
+ *******  It does so through updateAllFields()  *******
+ ******************************************************/
+function compute(label,calculation){
+  //var number = document.getElementById(label);
+  var inputValue = String(label.value); //Take the value of the input box as a string for testing
+  if (label.id == 'binary'){
+    convert = 2;
+  } else if (label.id == 'octal'){
+    convert = 8;
+  } else if (label.id == 'decimal'){
+    convert = 10;
+  } else if (label.id == 'hexadecimal'){
+    convert = 16;
+  }
+  if (calculation==true){
+    updateAllFields(inputValue, convert);
+  } else{
+    update(label.id,convert,inputValue,convert);
+  }
+}
+
+/****************************************************************
+ *******  updateAllFields() calculates all boxes at once. *******
+ *******  It is used when ANY value is tested           *********
+ ****************************************************************/
+function updateAllFields(inputValue,convert){
+  var value = [2, 8, 10, 16];
+  for (i = 0; i < input.length; i++){
+    var object = update(input[i],value[i],inputValue,convert);
+    if (object == 'error'){
+      alert('ERROR: Number limit reached\n'+
+            'Max inputs allowed are: \n'+
+            'Binary: 1111-1111-1111-1111\n'+
+            'Octal: 177,777\n'+
+            'Decimal: 65,535\n'+
+            'Hexadecimal: ffff');
+      return refresh();
+      break;
+    }
+  }
+}
+
+/**************************************************************
+ *******  update() calculates a single boxe's value.    *******
+ *******      It is used by updateAllFields()           *******
+ **************************************************************/
+function update(input,value,inputValue,convert){
+  var object = document.getElementById(input);
+  object.value = parseInt(inputValue,convert).toString(value);
+  if (object.selectionEnd > 16){
+    return 'error';
+  }
+  if (object.value == 'NaN'){
+    return refresh();
+  }
+}
+
+
+/*******************************************************
+ *******     refresh() resets all textboxes.     *******
+ *******  It is used when any box reaches "NaN"  *******
+ *******************************************************/
+
+function refresh(label){
+  for (i = 0; i < input.length; i++){
+    if (input[i] != label){ //Erase any field that wasn't just input
+                            //This means all fields if a button called the function
+      var refresher = document.getElementById(input[i]);
+      refresher.value = '';
+    }
+  }
+}
